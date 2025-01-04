78cf89bce315e33d0ef12747d45efeb6710b89df
Sujal Kapoor <2023uee0154@iitjammu.ac.in>
2025-01-04 10:47:51 +0530

    not completed

--- numpy.py
+++ numpy.py
@@ -1,2 +1,21 @@
-a=10
-b=20
+
+import numpy as np
+l = [1,2,3,4,5]
+l
+type(l)
+arr = np.array(l)
+arr
+np.asarray(l)
+arr1 = np.array([[1,2,3],[2,3,4]])
+arr1
+arr.ndim
+arr1.ndim
+l
+np.matrix(l)
+arr
+arr[0] = 50
+arr
+b = np.copy(arr)
+b[0]=34
+arr
+b
