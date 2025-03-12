4847cb6c531627323cfc515b77805b5aa4f6fb76
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-12 08:23:10 +0530

    Add files via uplpad

---
+++ polynomial_regression.py
@@ -0,0 +1,59 @@
+##import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline 
+X = 6 * np.random.rand(100, 1) - 3
+y =0.5 * X**2 + 1.5*X + 2 + np.random.randn(100, 1)
+# quadratic equation used- y=0.5x^2+1.5x+2+outliers
+plt.scatter(X,y,color='g')
+plt.xlabel('X dataset')
+plt.ylabel('Y dataset')
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+## Lets implement Simple Linear Regression
+from sklearn.linear_model import LinearRegression
+regression_1=LinearRegression()
+regression_1.fit(X_train,y_train)
+from sklearn.metrics import r2_score
+score=r2_score(y_test,regression_1.predict(X_test))
+print(score)
+## Lets visualize this model
+plt.plot(X_train,regression_1.predict(X_train),color='r')
+plt.scatter(X_train,y_train)
+plt.xlabel("X Dataset")
+plt.ylabel("Y")
+#LEts apply polynomial transformation
+from sklearn.preprocessing import PolynomialFeatures
+poly=PolynomialFeatures(degree=2,include_bias=True)
+X_train_poly=poly.fit_transform(X_train)
+X_test_poly=poly.transform(X_test)
+X_train_poly
+X_test_poly
+from sklearn.metrics import r2_score
+regression = LinearRegression()
+regression.fit(X_train_poly, y_train)
+y_pred = regression.predict(X_test_poly)
+score=r2_score(y_test,y_pred)
+print(score)
+print(regression.coef_)
+print(regression.intercept_)
+plt.scatter(X_train,regression.predict(X_train_poly))
+plt.scatter(X_train,y_train)
+poly=PolynomialFeatures(degree=3,include_bias=True)
+X_train_poly=poly.fit_transform(X_train)
+X_test_poly=poly.transform(X_test)
+X_train_poly
+
+#3 Prediction of new data set
+X_new = np.linspace(-3, 3, 200).reshape(200, 1)
+X_new_poly = poly.transform(X_new)
+X_new_poly
+y_new = regression.predict(X_new_poly)
+plt.plot(X_new, y_new, "r-", linewidth=2, label=" New Predictions")
+plt.plot(X_train, y_train, "b.",label='Training points')
+plt.plot(X_test, y_test, "g.",label='Testing points')
+plt.xlabel("X")
+plt.ylabel("y")
+plt.legend()
+plt.show()
