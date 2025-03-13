e54ee788542c66f5b85ee5e1e08145d95462b4cd
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-13 08:18:48 +0530

    Add files via upload

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -57,3 +57,27 @@ plt.xlabel("X")
 plt.ylabel("y")
 plt.legend()
 plt.show()
+
+#pipeline of polynomial and linear regression
+from sklearn.pipeline import Pipeline
+def poly_regression(degree):
+    X_new = np.linspace(-3, 3, 200).reshape(200, 1)
+    
+    poly_features=PolynomialFeatures(degree=degree,include_bias=True)
+    lin_reg=LinearRegression()
+    poly_regression=Pipeline([
+        ("poly_features",poly_features),
+        ("lin_reg",lin_reg)
+    ])
+    poly_regression.fit(X_train,y_train) ## ploynomial and fit of linear reression
+    y_pred_new=poly_regression.predict(X_new)
+    #plotting prediction line
+    plt.plot(X_new, y_pred_new,'r', label="Degree " + str(degree), linewidth=2)
+    plt.plot(X_train, y_train, "b.", linewidth=3)
+    plt.plot(X_test, y_test, "g.", linewidth=3)
+    plt.legend(loc="upper left")
+    plt.xlabel("X")
+    plt.ylabel("y")
+    plt.axis([-4,4, 0, 10])
+    plt.show()
+    
