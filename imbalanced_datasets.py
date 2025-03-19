1427ca64a071b62847b7f15ed890e33693306ad9
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-19 17:04:43 +0530

    Update imbalanced_datasets.py

--- imbalanced_datasets.py
+++ imbalanced_datasets.py
@@ -48,3 +48,34 @@ df_majority_downsampled = resample(df_majority,replace=False,
 df_majority_downsampled.shape
 df_downsampled = pd.concat([df_minority,df_majority_downsampled])
 df_downsampled['Target'].value_counts()
+
+
+#pip install seaborn
+import seaborn as sns
+import numpy as np
+import pandas as pd
+df = sns.load_dataset('titanic')
+df.isnull().sum()
+df.shape()
+df.dropna().shape
+sns.histplot(df['age'])
+# Mean value imputation
+df['age_mean']= df['age'].fillna(df['age'].mean())
+df[['age','age_mean']]
+sns.histplot(df['age_mean'],kde=True)
+
+# Median value imputation-if we have outliers in the dataset
+df['age_median'] = df['age'].fillna(df['age'].median())
+sns.scatterplot(df['age'])
+sns.scatterplot(df['age_median'])
+df[['age','age_mean','age_median']]
+df.isnull().sum()
+df['embarked'].unique()
+
+# Mode imputation teq-categorical values
+df[df['embarked'].isnull()]
+mode_value = df[df['embarked'].notna()]['embarked'].mode()[0]
+df['embarked_mode'] = df['embarked'].fillna(mode_value)
+df['embarked_mode'] = df['embarked'].fillna(mode_value)
+df['embarked_mode'].isnull().sum()
+df['embarked'].isnull().sum()
